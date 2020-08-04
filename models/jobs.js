172cd480165c4fbd4f38b50dd5bd23240f3bae38
Peter Hristakos <peter.hristakos@bigpond.com>
2020-08-04 10:10:52 +1000

    refactoring routes not to have sql in them

--- models/jobs.js
+++ models/jobs.js
@@ -4,17 +4,17 @@ const getSavedJobs = async userId => {
     let savedJobs = [];
     const results = await getSavedJobIdsByUserId(userId);
     for (let i = 0; i < results.rows.length; i++) {
-        const { rows } = await getJobsById(results.rows[i].api_id);
+        const { rows } = await getJobById(results.rows[i].api_id);
         savedJobs.push(rows[0]);
     }
     return savedJobs;
 }
 
-const getJobsById = async jobId => {
+const getJobById = async jobId => {
     const sql = "Select * FROM jobs WHERE api_id = $1";
     const params = [jobId];
-    const jobs = await runSql(sql, params);
-    return jobs;
+    const job = await runSql(sql, params);
+    return job;
 }
 
 const getSavedJobIdsByUserId = async userId => {
@@ -23,4 +23,43 @@ const getSavedJobIdsByUserId = async userId => {
     const savedJobIds = await runSql(sql, params);
     return savedJobIds;
 }
-module.exports = { getSavedJobs };
+
+const saveJobIdsForUserId = async (jobId, userId) => {
+    const sql = "INSERT INTO usertojobs (api_id, user_id) VALUES ($1,$2)";
+    const params = [jobId, userId];
+    const result = await runSql(sql, params);
+}
+
+const getJobs = async () => {
+    const sql = "SELECT * FROM jobs ORDER BY id ASC";
+    const params = [];
+    const jobs = await runSql(sql, params);
+    return jobs;
+}
+
+const saveJob = async job => {
+    const sql = `INSERT INTO jobs (api_id,type, url, created_at, company, company_url, location, title, description, how_to_apply, company_logo)
+    VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)`;
+    params = [
+        job.id,
+        job.type,
+        job.url,
+        job.created_at,
+        job.company,
+        job.company_url,
+        job.location,
+        job.title,
+        job.description,
+        job.how_to_apply,
+        job.company_logo
+    ]
+    console.log("saveJob");
+    await runSql(sql, params);
+}
+module.exports = {
+    getJobById,
+    getSavedJobs,
+    saveJobIdsForUserId,
+    getJobs,
+    saveJob
+};
