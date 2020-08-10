10f46af1f9186b3df25a584276d7424ae9514785
Shailesh <shlshkrk@gmail.com>
2020-08-10 10:29:59 +1000

    added logo to home page

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -1,10 +1,2 @@
-console.log("nav");
-// var nav = document.querySelector("nav");
-var links = document.querySelectorAll(".nav-link");
-for (var i = 0; i < links.length; i++) {
-    links[i].addEventListener("click", function (e) {
-        var current = document.querySelector(".active");
-        current.classList.remove("active");
-        e.target.classList.add("active");
-    });
-}
+var activeNavlink = document.querySelectorAll('nav a[href^="/' + location.pathname.split("/")[1] + '"]');
+activeNavlink[0].firstElementChild.classList.add('active');
